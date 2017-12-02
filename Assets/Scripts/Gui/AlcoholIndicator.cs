a4959f0378c80eb41454c0735f7893b1de03730d
Unknown <wsspad@gmail.com>
2017-12-02 15:56:20 +0100

    Added alcohol effects.

---
+++ Assets/Scripts/Gui/AlcoholIndicator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AlcoholIndicator : MonoBehaviour {
+
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Alcohol: " + PlayerStats.getAlcohol().ToString() + " ‰";
+	}
+}
---
+++ Assets/Scripts/Player/PlayerEffects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Pixelation.Scripts;
+using UnityEngine;
+
+public class PlayerEffects : MonoBehaviour {
+	public Pixelation mainCamera;
+	public Pixelation weaponCamera;
+
+	private float tick = 0.0f;
+
+	private void Update() {
+		// Alcohol effects
+		float alcohol = PlayerStats.getAlcohol() - 5.0f;
+
+		if (alcohol < 0.0f) alcohol = 0.0f;
+		if (alcohol > 20.0f) alcohol = 20.0f;
+
+		mainCamera.BlockCount = 512.0f - (alcohol / 20.0f) * 384.0f;
+		weaponCamera.BlockCount = 512.0f - (alcohol / 20.0f) * 384.0f;
+
+		tick += Time.deltaTime;
+
+		if (tick > 0.25f) {
+			tick -= 0.25f;
+
+			// Decrease alcohol
+			PlayerStats.removeAlcohol(0.1f);
+		}
+	}
+}
