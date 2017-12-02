94d210b625afb26834d072a9fe9d49584ee6ea23
Unknown <wsspad@gmail.com>
2017-12-02 14:51:54 +0100

    Enemy Stats

---
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+	// Max values
+	private const float maxHealth = 100.0f;
+	private const float maxArmor = 100.0f;
+
+	// You can tweak those
+	public float defaultArmor = 50.0f;
+
+	// Current stats
+	private float currentHealth = maxHealth;
+	private float currentArmor = 0.0f;
+
+	private void Start() {
+		currentArmor = defaultArmor;
+	}
+
+	public void dealDamage(float damage, bool direct = false) {
+		float dmg = damage;
+
+		if (!direct) {
+			dmg = damage * (1 - (currentArmor / maxArmor));
+
+			currentArmor -= damage;
+
+			if (currentArmor < 0.0f) currentArmor = 0.0f;
+		}
+
+		currentHealth -= dmg;
+
+		if (currentHealth < 0.0f) currentHealth = 0.0f;
+	}
+
+	public float getHealthPercent() { return currentHealth / maxHealth; }
+}
