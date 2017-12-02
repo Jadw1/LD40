bf370ca6c7fd92c3d79a4d1ce0f965e3465db07b
Jadw1 <jadw1_1@o2.pl>
2017-12-02 14:27:00 +0100

    changed weapon system

---
+++ Assets/Scripts/Weapon/Riffle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Riffle : MonoBehaviour {
+
+    public float range = 50.0f;
+
+    private void FixedUpdate() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Input.GetButtonDown("Fire1")) {
+
+        }
+    }
+}
