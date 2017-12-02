17eee44e68afc7dd0c7f6df7c385b850bd65cd51
Unknown <wsspad@gmail.com>
2017-12-02 14:41:10 +0100

    Changed the pickup script

--- Assets/Scripts/Items/ItemPickUp.cs
+++ Assets/Scripts/Items/ItemPickUp.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class ItemPickUp : MonoBehaviour {
 	public enum Type {
-		VODKA
+		ALCOHOL,
+		HEAL,
+		ARMOR,
+		DAMAGE,
+		DAMAGE_DIRECT
 	};
+
+	public float strength = 1.0f;
 
 	public Type itemType;
 
 	private void OnTriggerEnter(Collider other) {
 		if (other != null && other.tag == "Player") {
-			PlayerStats.addAlcohol(1.0f);
-			Destroy(this.gameObject);
+			switch (itemType) {
+				case Type.ALCOHOL:
+					PlayerStats.addAlcohol(strength);
+					Destroy(this.gameObject);
+					break;
+				case Type.HEAL:
+					PlayerStats.heal(strength);
+					Destroy(this.gameObject);
+					break;
+				case Type.DAMAGE:
+					PlayerStats.dealDamage(strength);
+					Destroy(this.gameObject);
+					break;
+				case Type.DAMAGE_DIRECT:
+					PlayerStats.dealDamage(strength, true);
+					Destroy(this.gameObject);
+					break;
+				case Type.ARMOR:
+					PlayerStats.addArmor(strength);
+					Destroy(this.gameObject);
+					break;
+
+				default: break;
+			}
 		}
 	}
 }
