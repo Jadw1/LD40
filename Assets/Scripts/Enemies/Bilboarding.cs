4d6402a45fb122a6f2b8cc84e3c00413755c5e68
Jadw1 <jadw1_1@o2.pl>
2017-12-02 13:04:41 +0100

    add bilboarding

---
+++ Assets/Scripts/Enemies/Bilboarding.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Bilboarding : MonoBehaviour {
+
+    private Vector3 direction;
+	
+	void Update () {
+        direction = Camera.main.transform.forward;
+        direction.y = 0.0f;
+
+        transform.rotation = Quaternion.LookRotation(direction);        
+	}
+}
