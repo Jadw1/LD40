9f1c428afda7c03c4641417140bbb20b8dfa4b78
Jadw1 <jadw1_1@o2.pl>
2017-12-02 16:24:18 +0100

    some shooting fixes

--- Assets/Scripts/Weapon/Riffle.cs
+++ Assets/Scripts/Weapon/Riffle.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Riffle : MonoBehaviour {
 
+    public float damage = 40.0f;
     public float range = 50.0f;
     public float fireRate = 10.0f;
 
     private float timeToFire = 0.0f;
-
+    
     public GameObject bulletHole;
+    public LayerMask ignore;
 
     private void FixedUpdate() {
         Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0.0f));
@@ -17,7 +19,10 @@ public class Riffle : MonoBehaviour {
         if(Input.GetButton("Fire1") && Time.time > timeToFire) {
             timeToFire = Time.time + 1 / fireRate;
 
-            if(Physics.Raycast(ray, out hit, range)) {
+            if(Physics.Raycast(ray, out hit, range, ~ignore)) {
+                if(hit.collider.tag == "Enemy") {
+
+                }
                 Instantiate(bulletHole, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
             }
         }
