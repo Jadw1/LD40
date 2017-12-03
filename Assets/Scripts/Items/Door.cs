8cb8ca8d5ac5d2359a84ae19a838d4edd5cbd2ca
Jadw1 <jadw1_1@o2.pl>
2017-12-03 14:04:11 +0100

    partly done door interact

--- Assets/Scripts/Items/Door.cs
+++ Assets/Scripts/Items/Door.cs
@@ -14,29 +14,63 @@ public class Door : MonoBehaviour {
     public float speed = 5.0f;
     public float offset = 2.5f;
     private float diff = 0.0f;
+    private Vector3 pos;
 
     public float timeToClose = 10.0f;
     private float timer = 0.0f;
     private bool isOpened = false;
     private bool isOpening = false;
+    private bool isClosing = false;
 
     public void Open() {
+        Debug.Log("aaaaaa");
+
         if (isOpened)
             return;
 
         isOpening = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void Start() {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		if(isOpening) {
             diff += offset * speed * Time.deltaTime;
+            if (diff > offset) {
+                diff = offset;
+                isOpening = false;
+                isOpened = true;
+            }
 
             if(direction == DoorOpeningDirection.LEFT) {
-
+                transform.position = pos + new Vector3(0.0f, 0.0f, diff);
+            }
+            else if(direction == DoorOpeningDirection.RIGHT) {
+                transform.position = pos - new Vector3(0.0f, 0.0f, diff);
             }
         }
-
-
+        else if(isOpened) {
+            timer += Time.deltaTime;
+            if(timer >= timeToClose) {
+                isOpening = false;
+                isClosing = true;
+            }
+        }
+        else if(isClosing) {
+            diff -= offset * speed * Time.deltaTime;
+            if(diff < 0.0f) {
+                diff = 0.0f;
+                isClosing = false;
+            }
+            if (direction == DoorOpeningDirection.LEFT) {
+                transform.position = pos - new Vector3(0.0f, 0.0f, diff);
+            }
+            else if (direction == DoorOpeningDirection.RIGHT) {
+                transform.position = pos + new Vector3(0.0f, 0.0f, diff);
+            }
+        }
 	}
 }
---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour {
+
+    public float interactRange = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetButtonDown("Interact")) {
+            Debug.Log("as");
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0.0f));
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, interactRange)) {
+                Debug.Log(hit.collider.name);
+                if(hit.collider.tag == "Door") {
+                    Door door = hit.collider.GetComponent<Door>();
+                    if(door != null) {
+                        door.Open();
+                    }
+                    else {
+                        Debug.LogError("ERROR!");
+                    }
+                }
+            }
+        }		
+	}
+}
