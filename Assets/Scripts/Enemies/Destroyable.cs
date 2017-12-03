cb56d0c0c8584e90f2ca458628e12c45f448fbc0
Unknown <wsspad@gmail.com>
2017-12-03 22:33:42 +0100

    Added Destroyables

---
+++ Assets/Scripts/Enemies/Destroyable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyStats))]
+public class Destroyable : MonoBehaviour {
+	private EnemyStats stats;
+
+	private void Start() {
+		stats = GetComponent<EnemyStats>();
+	}
+
+	private void Update() {
+		if (stats.getHealthPercent() <= 0.0f) Destroy(this.gameObject);
+	}
+}
