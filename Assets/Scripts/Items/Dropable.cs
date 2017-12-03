61b71e2f9ce7bda4e17359bf3efad323e6b23c87
Jadw1 <jadw1_1@o2.pl>
2017-12-03 23:25:08 +0100

    wooden chest & unfinished dropping system

---
+++ Assets/Scripts/Items/Dropable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropable : MonoBehaviour {
+
+    public List<GameObject> items;
+
+    public List<int> weights;
+
+    private void Awake() {
+        items = new List<GameObject>();
+        weights = new List<int>();
+    }
+
+    private void OnDestroy() {
+        int all = AllWeights();
+        int rand = Random.Range(0, all);
+
+        for(int i=0; i<items.Count; i++) {
+            if(rand >= AllWeightsTo(i) && rand < AllWeightsTo(i+1)) {
+                Drop();
+                break;
+            }
+        }
+    }
+
+    private int AllWeights() {
+        int sum = 0;
+        for(int i=0; i<items.Count; i++) {
+            sum += weights[i];
+        }
+        return sum;
+    }
+
+    private int AllWeightsTo(int count) {
+        int sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum += weights[i];
+        }
+        return sum;
+    }
+
+    private void Drop() {
+
+    }
+}
