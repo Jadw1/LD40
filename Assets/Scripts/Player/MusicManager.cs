15a4f2dcc101c446a5ba55604958d151e74eef80
Unknown <wsspad@gmail.com>
2017-12-04 20:51:57 +0100

    Epic music

---
+++ Assets/Scripts/Player/MusicManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour {
+	public AudioClip epicMusic;
+	public AudioClip normalMusic;
+
+	private AudioSource audio;
+
+	public static void StartEpicMusic() {
+		if (manager != null) {
+			manager.PlayEpicMusic();
+		}
+	}
+
+	private static MusicManager manager;
+
+	private void Start() {
+		manager = this;
+
+		audio = GetComponent<AudioSource>();
+
+		audio.clip = epicMusic;
+		audio.Play();
+	}
+
+	private void Update() {
+		if (!audio.isPlaying) {
+			audio.clip = normalMusic;
+			audio.Play();
+		}
+	}
+
+	public void PlayEpicMusic() {
+		if (audio.isPlaying) audio.Stop();
+
+		audio.clip = epicMusic;
+		audio.Play();
+	}
+}
