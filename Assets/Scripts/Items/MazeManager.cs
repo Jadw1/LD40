4ff9f40399260f5d560142d53d092cd4cf91e089
Jadw1 <jadw1_1@o2.pl>
2017-12-04 19:15:52 +0100

    maze

---
+++ Assets/Scripts/Items/MazeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeManager : MonoBehaviour {
+
+    private Transform spawnPoint;
+    public GameObject drunkRoad;
+
+    private void Awake() {
+        spawnPoint = transform.Find("Spawn Point");
+    }
+
+    private void Start() {
+        Spawn();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Spawn();
+    }
+
+    private void Spawn() {
+        Transform clone = Instantiate(drunkRoad, spawnPoint.position, spawnPoint.rotation).transform;
+        clone.parent = transform;
+    }
+}
