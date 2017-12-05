25ae20e0f37e669908fc51867da6f6cc1ccd0bdb
Jadw1 <jadw1_1@o2.pl>
2017-12-05 20:58:27 +0100

    help screen

---
+++ Assets/Scripts/Gui/HelpScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpScreen : MonoBehaviour {
+
+    public GameObject helpScreen;
+
+    private bool isActive = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private void Update () {
+		if(Input.GetButton("Help")) {
+            Activate();
+        }
+        else {
+            Deactivate();
+        }
+	}
+
+    private void Activate() {
+        isActive = true;
+        helpScreen.SetActive(true);
+    }
+
+    private void Deactivate() {
+        isActive = false;
+        helpScreen.SetActive(false);
+    }
+}
